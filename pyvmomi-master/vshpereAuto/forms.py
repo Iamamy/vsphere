abb9885f977f2c31b9da4fc1a4a0323d52b7c7a1
Amy Li <ali@splunk.com>
2015-06-29 13:47:35 +0800

    mask password

--- pyvmomi-master/vshpereAuto/forms.py
+++ pyvmomi-master/vshpereAuto/forms.py
@@ -1,15 +1,15 @@
 __author__ = 'amy1'
 
 from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField, validators, SelectField
+from wtforms import StringField, SubmitField, validators, SelectField, PasswordField
 oper_choices = [('createsnapshot','createsnapshot'),('revertsnapshot','revertsnapshot'),('deletesnapshot','deletesnapshot')]
 
 
 class vshpereForm(Form):
-    host = StringField("Vsphere host")
-    port = StringField("Vsphere port")
+    host = StringField("Vsphere host", default='vm.splunk.io')
+    port = StringField("Vsphere port", default='443')
     user = StringField("Vsphere user")
-    password = StringField("Vsphere password")
+    password = PasswordField("Vsphere password")
     operation = SelectField("Operation", choices=oper_choices)
     vappname = StringField("Vapp name")
     snapname = StringField("Snapshot name")
